73157682b7cd8f26daa41875590b342d239b8cc5
willster277 <johnsonseducational@gmail.com>
2021-07-12 13:50:58 +0100

    418 I'm a teapot

--- TS/src/index.ts
+++ TS/src/index.ts
@@ -4,6 +4,7 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import path from "path";
 
 import * as routes from "./routes/";
 
@@ -13,21 +14,47 @@ const PORT = 5000;
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true }));
 
+// customize your 404 webpage here
+const defaultErr = (req: Request, res: Response) => {
+	res.status(404).send("This page or resource doesn't exist.");
+};
+// alternatively, you can annoy your frontend dev with this
+/*
+	const defaultErr = (req: Request, res: Response) => {
+		res.status(418).send(
+			`<div style="width: 80vw; height: 80vh; margin: 10vh 10vw;"><img alt="${
+				req.body.errMsg ? req.body.errMsg : `Could not ${req.method} ${req.path}`
+			}" src="assets/418.gif" style="width: 100%; max-height: 100%"></div>`,
+		);
+	};
+*/
+
+// access to assets if needed
+// this doesn't require a route as the only acceptable request here is GET
+/*
+	app.get("/assets/*", (req: Request, res: Response) => {
+		try {
+			res.status(200).sendFile(path.normalize(`/${__dirname}/${req.path}`));
+		} catch (err) {
+			req.body.errMsg = err.message;
+			defaultErr(req, res);
+		}
+	});
+*/
+
 // use your routes here
 app.use("/template", routes.template);
 
 // no routes beyond this point
-// customize your 404 webpage here
-app.get("*", (req: Request, res: Response) => {
-	res.status(404).send("This page or resource doesn't exist.");
-});
+app.get("*", defaultErr);
 
 // allows using independent frontend
+// be sure to set your proxy if you use this
 app.use(cors());
 
 // connect to mongoDB and go live
 mongoose
-	.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+	.connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, autoCreate: true })
 	.then(() => {
 		app.listen(PORT, () => {
 			console.log(`Server running on port ${PORT}`);
